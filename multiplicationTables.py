33786df44875e7cd0ec43a1e7cb5bdc04bb68e52
Roberto <robert_garcia10@hotmail.com>
2014-11-20 19:48:23 -0600

    Revert "Python"
    This reverts commit 2ffcacb21de1857788850e3b6f2a115cc767d027.

--- multiplicationTables.py
+++
@@ -1,6 +0,0 @@
-matriz = []
-for renglon in range(3):
-    matriz.append([])
-    for columna in range(12):
-        matriz[renglon].append( (columna+1)*(renglon+1) )
-    print '%4d%4d%4d%4d%4d%4d%4d%4d%4d%4d%4d%4d'%(matriz[0][renglon],matriz[renglon][1],matriz[renglon][2],matriz[renglon][3],matriz[renglon][4],matriz[renglon][5],matriz[renglon][6],matriz[renglon][7],matriz[renglon][8],matriz[renglon][9],matriz[renglon][10],matriz[renglon][11])
